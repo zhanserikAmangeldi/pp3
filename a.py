a04964f2f7d98a20953c2670b9ecc166756f7dc7
zhanserikAmangeldi <amangeldi.janserik2017@gmail.com>
2023-02-07 23:54:24 +0600

    second

--- a.py
+++ a.py
@@ -11,5 +11,19 @@ def binary_search(list, end, x):
         elif list[mid] < x:
             start = mid + 1
     return None
+def recursive_binary_search(list,start, end, x):
+    counter =0
+
+    if start <= end:
+        mid = int((start + end)/2)
+        counter +=1
+        if list[mid] == x:
+            return mid
+        elif list[mid] > x:
+            return recursive_binary_search(list,start, mid -1, x)
+        elif list[mid] < x:
+            return recursive_binary_search(list,mid + 1, end, x)
+    else:
+        return None
 S= [x for x in range(30, 100000)]
-print(binary_search(S, len(S) - 1, 33))
+print(recursive_binary_search(S,0, len(S) - 1, 33))
